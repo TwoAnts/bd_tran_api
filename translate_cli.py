22101d6bf07e38229a93c3d6edf1da5015b4d832
lzmyhzy <lzmyhzy@gmail.com>
2017-03-15 15:17:13 +0800

    Support quotation marks when sentence has \n

--- translate_cli.py
+++ translate_cli.py
@@ -14,11 +14,25 @@ if __name__ == '__main__':
     print('Start trans from %s to %s:' %(client.get_option('from_lang'),\
             client.get_option('to_lang'))\
          )
-         
+    
     while True:
         in_str = input('>>> ')
         
-        in_str = in_str.strip() if in_str is not None else None
+        in_str = in_str.strip()
+        #When your sentence has \n, use " or '. 
+        if in_str and in_str[0] in ('\'', '\"'):
+            quote = in_str[0]
+            str_list = [in_str]
+            while True:
+                in_str = input('>    ')
+                in_str = in_str.strip()
+                str_list.append(in_str)
+                if in_str and in_str[-1] == quote:
+                    break
+            
+            in_str = ' '.join(str_list)[1:-1]
+            
+        
         if in_str in ('exit', 'quit'): break
         if not in_str: continue 
         
