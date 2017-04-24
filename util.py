0830dafa36583ff305cb3322674c499272c1763e
lzmyhzy <lzmyhzy@gmail.com>
2017-04-24 09:16:53 +0800

    Use full path of config.txt.

--- util.py
+++ util.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding : utf-8 -*-
 
+import os
 CONFIG_FILE = 'config.txt'
+CUR_DIR = os.path.abspath(os.path.dirname(__file__))
+CF_PATH = os.path.join(CUR_DIR, CONFIG_FILE)
 
 #You need a baidu translate <appid> and <secret_key> 
 #config.txt -> 
 #APPID = xxxxxxx
 #SECRET_KEY = xxxxxx
 
-def load_config(fn=CONFIG_FILE):
+def load_config(fn=CF_PATH):
     config = {}
     
     with open(fn, 'r', encoding='utf-8') as f:
